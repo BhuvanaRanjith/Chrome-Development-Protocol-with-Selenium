5916ad1019a793d8382f8372c6e5b306c2ebcf6b
Bhuvana Ranjith <mailtobhuvana04@gmail.com>
2023-06-08 15:44:18 -0700

    New Code has been added. Set up the driver. Set up the window into mobile mode. Click on the Hamburger button and goto library page.

--- src/main/java/MobileEmulatorTest.java
+++ src/main/java/MobileEmulatorTest.java
@@ -5,12 +5,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v85.emulation.Emulation;
+import org.testng.annotations.Test;
 
 import java.util.Optional;
 
 public class MobileEmulatorTest {
     public static void main(String[] args) throws InterruptedException {
-        // WebDriverManager.chromedriver().setup();
+
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\IamBh\\IdeaProjects\\SeleniumFeatureLatest\\src\\Driver\\chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
@@ -22,9 +23,11 @@ public class MobileEmulatorTest {
         //send commends to CDP Methods-> CDP methods will invoke and get access to chrome dev tools
         devTools.send(Emulation.setDeviceMetricsOverride(600, 1000, 50, true, Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty()));
         driver.get("https://rahulshettyacademy.com/angularAppdemo/");
-        Thread.sleep(3000);
+
         driver.findElement(By.cssSelector(".navbar-toggler-icon")).click();
+        Thread.sleep(3000);
         driver.findElement(By.xpath("//a[normalize-space()='Library']")).click();
+
 
         System.out.println(driver.getTitle());
         driver.close();
