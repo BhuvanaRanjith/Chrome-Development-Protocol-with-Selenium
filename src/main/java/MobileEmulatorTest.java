3529b40d3a7f323174afeb814bd435948965d64a
Bhuvana Ranjith <mailtobhuvana04@gmail.com>
2023-06-08 14:46:35 -0700

    New Code has been added. Set up the driver. Set up the window into mobile mode. Click on the Hamburger button and goto library page.

--- src/main/java/MobileEmulatorTest.java
+++ src/main/java/MobileEmulatorTest.java
@@ -1,17 +1,33 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.By;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v85.emulation.Emulation;
+
+import java.util.Optional;
 
 public class MobileEmulatorTest {
-    public static void main(String[] args)
-    {
-       // WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\IamBh\\IdeaProjects\\SeleniumFeatureLatest\\src\\Driver\\chromedriver.exe");
-        ChromeDriver driver=new ChromeDriver();
-        DevTools devTools=driver.getDevTools();
+    public static void main(String[] args) throws InterruptedException {
+        // WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\IamBh\\IdeaProjects\\SeleniumFeatureLatest\\src\\Driver\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver = new ChromeDriver(options);
+
+
+        DevTools devTools = driver.getDevTools();
         devTools.createSession();
         //send commends to CDP Methods-> CDP methods will invoke and get access to chrome dev tools
+        devTools.send(Emulation.setDeviceMetricsOverride(600, 1000, 50, true, Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty()));
+        driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector(".navbar-toggler-icon")).click();
+        driver.findElement(By.xpath("//a[normalize-space()='Library']")).click();
+
+        System.out.println(driver.getTitle());
+        driver.close();
 
     }
 }
