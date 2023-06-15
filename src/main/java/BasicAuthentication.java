8f0f3230977bd0d6d8cccb34e1bc87079b26cc87
Bhuvana Ranjith <mailtobhuvana04@gmail.com>
2023-06-15 11:40:08 -0700

    Using selenium provide the username and password for Block authentication popup in windows.

---
+++ src/main/java/BasicAuthentication.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.HasAuthentication;
+import org.openqa.selenium.UsernameAndPassword;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.testng.annotations.Test;
+
+import java.net.URI;
+import java.util.function.Predicate;
+
+@Test
+public class BasicAuthentication {
+    public void BasicAuthentication()
+    {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\IamBh\\IdeaProjects\\SeleniumFeatureLatest\\src\\Driver\\chromedriver.exe");
+        ChromeOptions options=new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver=new ChromeDriver(options);
+
+        Predicate<URI> uriPredicate= uri->uri.getHost().contains("httpbin.org");
+         ((HasAuthentication)driver).register(uriPredicate, UsernameAndPassword.of("foo","bar"));
+        driver.get("http://httpbin.org/basic-auth/foo/bar");
+        driver.close();
+
+    }
+}
